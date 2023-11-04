94945e11af8a187f03da02ebee057f81694562da
ifilot <ivo@ivofilot.nl>
2023-11-04 11:04:45 +0100

    Implementing file progression

--- cassette-utility/main.c
+++ cassette-utility/main.c
@@ -26,19 +26,30 @@ int main(void) {
     uint8_t line = 3;
     uint8_t iter = 0;
 
-    while(iter < 12) {
+    while(memory[CASSTAT] != 'M' && iter < 20) {
         sprintf(&vidmem[0x50*line], "%04i", iter+1);
         tape_read_block();
 
         // copy data from description to screen
         memcpy(&vidmem[0x50*line+5], &memory[DESC1], 8);
         memcpy(&vidmem[0x50*line+13], &memory[DESC2], 8);
+        uint16_t length = (uint16_t)memory[LENGTH] | ((uint16_t)memory[LENGTH+1] << 8);
+        sprintf(&vidmem[0x50*line+22], "%05i", length);
+        sprintf(&vidmem[0x50*line+28], "%02i", memory[BLOCKCTR]);
+
+        // consume all blocks
+        while(memory[BLOCKCTR] > 1) {
+            sprintf(&vidmem[0x50*line+31], "%02i", memory[BLOCKCTR]-1);
+            tape_read_block();
+        }
+        vidmem[0x50*line+30] = COL_GREEN;
+        sprintf(&vidmem[0x50*line+31], "OK");
 
         line++;
         iter++;
 
         clearline(1);
-        sprintf(&vidmem[0x50*1], "Press any key to read the next block.");
+        sprintf(&vidmem[0x50*1], "Press any key to read the next file.");
 
         keymem[0x0C] = 0;
         while(keymem[0x0C] == 0) {} // wait until a key is pressed
