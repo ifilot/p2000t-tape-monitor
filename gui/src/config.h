430b409dbeb26aa512f9cb6afab9d42d7557936c
ifilot <ivo@ivofilot.nl>
2024-03-17 21:49:06 +0100

    Incrementing version number

--- gui/src/config.h
+++ gui/src/config.h
@@ -22,7 +22,7 @@
 #define _CONFIG_H
 
 #define PROGRAM_NAME "P2000T FAT READER"
-#define PROGRAM_VERSION "0.5.0"
+#define PROGRAM_VERSION "0.5.1"
 #define ICON_PATH ":/assets/icons/fat_flasher_128px.png"
 
 #define UNUSED(x) (void)(x)
